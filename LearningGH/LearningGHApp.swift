efc01d0dcaa75af67a1289790d6c5550f1aa3f27
joh <joh@ihmc.org>
2022-12-05 10:22:46 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jae O. on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
